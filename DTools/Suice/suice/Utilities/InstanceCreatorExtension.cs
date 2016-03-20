1b4b7410007b8c6407e560092f553e268e2f737b
Rohit Bhosle <hirohitthegreat@gmail.com>
2016-03-21 00:09:35 +0530

    Dictornary type changed to remove casting. changes to format and code arrangement, to be in sync with the rest of Coop's code style

--- DTools/Suice/suice/Utilities/InstanceCreatorExtension.cs
+++ DTools/Suice/suice/Utilities/InstanceCreatorExtension.cs
@@ -4,68 +4,80 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
-/// <summary>
-/// Got this from the following sites :
-/// https://vagifabilov.wordpress.com/2010/04/02/dont-use-activator-createinstance-or-constructorinfo-invoke-use-compiled-lambda-expressions/
-/// https://ayende.com/blog/3167/creating-objects-perf-implications
-/// http://rogeralsing.com/2008/02/28/linq-expressions-creating-objects/
-/// https://rboeije.wordpress.com/2012/04/19/extension-method-on-type-as-alternative-for-activator-createinstance/ - Implemented this
-/// 
-/// This extension is a benchmarked better implementation to Activator.CreateInstance when taking the second link into consideration
-/// Here the expression is further cached so as to better instantiation for factory implementations
-/// </summary>
-internal static class InstanceCreatorExtensions
+namespace DTools.Suice
 {
-    internal static object New(this Type type, params object[] args)
+    /// <summary>
+    /// Got this from the following sites :
+    /// https://vagifabilov.wordpress.com/2010/04/02/dont-use-activator-createinstance-or-constructorinfo-invoke-use-compiled-lambda-expressions/
+    /// https://ayende.com/blog/3167/creating-objects-perf-implications
+    /// http://rogeralsing.com/2008/02/28/linq-expressions-creating-objects/
+    /// https://rboeije.wordpress.com/2012/04/19/extension-method-on-type-as-alternative-for-activator-createinstance/ - Implemented this
+    /// 
+    /// This extension is a benchmarked better implementation to Activator.CreateInstance when taking the second link into consideration
+    /// Here the expression is further cached so as to better instantiation for factory implementations
+    /// 
+    /// @author ChicK00o
+    /// </summary>
+    internal static class InstanceCreatorExtensions
     {
-        Type[] argTypes = args.Select(a => a.GetType()).ToArray();
+        private const string PARAM_NAME = "args";
 
-        ConstructorInfo constructorInfo = type.GetConstructor(argTypes);
-        ObjectActivator activator = GetActivator(constructorInfo);
+        private static Dictionary<ConstructorInfo, ObjectActivator> activators =
+            new Dictionary<ConstructorInfo, ObjectActivator>();
 
-        return activator(args);
-    }
+        private delegate object ObjectActivator(params object[] args);
 
-    internal delegate object ObjectActivator(params object[] args);
-
-    private static Dictionary<ConstructorInfo, object> _activators = new Dictionary<ConstructorInfo, object>();
-
-    internal static ObjectActivator GetActivator(ConstructorInfo constructorInfo)
-    {
-        object activator;
-        if (_activators.TryGetValue(constructorInfo, out activator)) return (ObjectActivator)activator;
-
-        ParameterInfo[] paramsInfo = constructorInfo.GetParameters();
-
-        //create a single param of type object[]
-        ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
-
-        Expression[] argsExp = new Expression[paramsInfo.Length];
-
-        //pick each arg from the params array
-        //and create a typed expression of them
-        for (int i = 0; i < paramsInfo.Length; i++)
+        internal static object New(this Type type, params object[] args)
         {
-            Expression index = Expression.Constant(i);
+            Type[] argTypes = args.Select(a => a.GetType()).ToArray();
 
-            Expression paramAccessorExp = Expression.ArrayIndex(param, index);
+            ConstructorInfo constructorInfo = type.GetConstructor(argTypes);
+            ObjectActivator activator = GetActivator(constructorInfo);
 
-            Expression paramCastExp = Expression.Convert(paramAccessorExp, paramsInfo[i].ParameterType);
-
-            argsExp[i] = paramCastExp;
+            return activator(args);
         }
 
-        //make a NewExpression that calls the constructor with the args we just created
-        NewExpression newExp = Expression.New(constructorInfo, argsExp);
+        private static ObjectActivator GetActivator(ConstructorInfo constructorInfo)
+        {
+            ObjectActivator activator;
 
-        //create a lambda with the NewExpression as body and our param object[] as arg
-        LambdaExpression lambda = Expression.Lambda(typeof(ObjectActivator), newExp, param);
+            if(activators.TryGetValue(constructorInfo, out activator) == false) {
+                activator = CreateNewActivator(constructorInfo);
+            }
 
-        //compile it
-        ObjectActivator compiled = (ObjectActivator)lambda.Compile();
+            return activator;
+        }
 
-        _activators.Add(constructorInfo, compiled);
+        private static ObjectActivator CreateNewActivator(ConstructorInfo constructorInfo)
+        {
+            ParameterInfo[] paramsInfo = constructorInfo.GetParameters();
 
-        return compiled;
+            //create a single param of type object[]
+            ParameterExpression param = Expression.Parameter(typeof(object[]), PARAM_NAME);
+
+            Expression[] argsExp = new Expression[paramsInfo.Length];
+
+            //pick each arg from the params array
+            //and create a typed expression of them
+            for(int i = 0; i < paramsInfo.Length; i++) {
+                Expression index = Expression.Constant(i);
+                Expression paramAccessorExp = Expression.ArrayIndex(param, index);
+                Expression paramCastExp = Expression.Convert(paramAccessorExp, paramsInfo[i].ParameterType);
+                argsExp[i] = paramCastExp;
+            }
+
+            //make a NewExpression that calls the constructor with the args we just created
+            NewExpression newExp = Expression.New(constructorInfo, argsExp);
+
+            //create a lambda with the NewExpression as body and our param object[] as arg
+            LambdaExpression lambda = Expression.Lambda(typeof(ObjectActivator), newExp, param);
+
+            //compile it
+            ObjectActivator compiled = (ObjectActivator) lambda.Compile();
+
+            activators.Add(constructorInfo, compiled);
+
+            return compiled;
+        }
     }
 }
